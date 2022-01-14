5367c92c0ccf04e0380315e195b26d80e9276803
DrDesten <70536771+DrDesten@users.noreply.github.com>
2022-01-14 20:08:44 +0100

    fixed normalizedTime, improved compatibility

--- shaders/lib/composite_basics.glsl
+++ shaders/lib/composite_basics.glsl
@@ -4,11 +4,6 @@ uniform sampler2D depthtex0; // Depth
 uniform vec2 screenSize;
 uniform vec2 screenSizeInverse;
 
-#define gl_FragData[0] gl_FragData[0]
-#define gl_FragData[1] gl_FragData[1]
-#define FD2 gl_FragData[2]
-#define FD3 gl_FragData[3]
-
 vec3 getAlbedo(vec2 coord) {
     return texture2D(colortex0, coord).rgb;
 }
