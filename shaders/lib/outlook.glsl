b9345131600565b80376b1401f5dc6372123f6e9
DrDesten <>
2022-05-17 00:46:54 +0200

    added options to settings menu

--- shaders/lib/outlook.glsl
+++ shaders/lib/outlook.glsl
@@ -1,5 +1,5 @@
 if (sneaking > 1e-6) {
-    const float scaleFactor = -pow(SNEAK_OUTLOOK_RADIUS, -4);
+    const float scaleFactor = -pow(SNEAK_OUTLOOK_RADIUS + 1e-30, -4);
     vec3 player = toPlayer(toView(gl_Position.xyz / gl_Position.w));
     player.y   -= SNEAK_OUTLOOK_HEIGHT * (1 - exp(sq(sqmag(vec3(player.x, player.y - 1, player.z))) * scaleFactor)) * sneaking;
     gl_Position = playerToClip(player);
