681d190010addb4ec765ebfe6f466351b7350e9c
DrDesten <>
2022-05-17 00:21:22 +0200

    added sneak outlook radius and height options

--- shaders/lib/outlook.glsl
+++ shaders/lib/outlook.glsl
@@ -1,5 +1,6 @@
 if (sneaking > 1e-6) {
+    const float scaleFactor = -pow(SNEAK_OUTLOOK_RADIUS, -4);
     vec3 player = toPlayer(toView(gl_Position.xyz / gl_Position.w));
-    player.y   -= 10 * (1 - exp(sq(sqmag(vec3(player.x, player.y - 1, player.z))) * -0.004)) * sneaking;
+    player.y   -= SNEAK_OUTLOOK_HEIGHT * (1 - exp(sq(sqmag(vec3(player.x, player.y - 1, player.z))) * scaleFactor)) * sneaking;
     gl_Position = playerToClip(player);
 }
