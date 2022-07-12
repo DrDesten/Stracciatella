51be6310405804038eebeddfb3081464cbd18bad
DrDesten <>
2022-07-12 17:54:05 +0200

    stuff

--- shaders/lib/bufferformats.glsl
+++ shaders/lib/bufferformats.glsl
@@ -16,11 +16,15 @@ const int colortex5Format = RGB8;   // EmissiveColor
 
 */
 
-
+#ifdef OVERWORLD
+const vec4 colortex1ClearColor = vec4(0,0,0,0);
+#endif
 const vec4 colortex3ClearColor = vec4(0,0,0,0);
 const vec4 colortex5ClearColor = vec4(0,0,0,0);
 
+#if MC_VERSION < 11900
 const bool colortex0Clear = false;
+#endif
 const bool colortex1Clear = false;
 const bool colortex2Clear = false;
 const bool colortex3Clear = true;
