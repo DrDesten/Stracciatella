7e54c8edb4fafbbdc9b51cd5ce10e84c76ae6738
DrDesten <70536771+DrDesten@users.noreply.github.com>
2022-12-03 18:12:17 +0100

    stuff

--- src/lib/setup.glsl
+++ src/lib/setup.glsl
@@ -1,17 +1,21 @@
 /*
-const int colortex0Format = RGBA8;  // Color
-const int colortex1Format = RG16;   // Lightmap, AO, Emissiveness (encoded)
-const int colortex2Format = R8;     // Unused (LUT)
+// Main Color Buffer
+const int colortex0Format = RGBA8;
+// Lightmap Values, AO and Emissiveness information ( encoded to RG16 ) 
+const int colortex1Format = RG16;
+// Bound to LUT in composite ( unused otherwise )
+const int colortex2Format = R8;
+// Rain Stencil ( for rain refraction )
 const int colortex3Format = R8;     // Effects
-const int colortex4Format = RGBA16; // LightmapColor + Depth
-const int colortex5Format = RGB8;   // EmissiveColor
+// Colored Lights Data ( RGB: Color, A: Depth ) ( fixed resolution )
+const int colortex4Format = RGBA16;
+// Emissive Colors ( used for colored lights )
+const int colortex5Format = RGB8;
 */
 
 #ifdef OVERWORLD
 const vec4 colortex1ClearColor = vec4(0,0,0,0);
 #endif
-const vec4 colortex3ClearColor = vec4(0,0,0,0);
-const vec4 colortex5ClearColor = vec4(0,0,0,0);
 
 #if MC_VERSION < 11900
 const bool colortex0Clear = false;
@@ -19,5 +23,9 @@ const bool colortex0Clear = false;
 const bool colortex1Clear = false;
 const bool colortex2Clear = false;
 const bool colortex3Clear = true;
+const vec4 colortex3ClearColor = vec4(0,0,0,0);
+#ifdef COLORED_LIGHTS
 const bool colortex4Clear = false;
 const bool colortex5Clear = true;
+const vec4 colortex5ClearColor = vec4(0,0,0,0);
+#endif
