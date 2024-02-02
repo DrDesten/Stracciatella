63be00512923707fbb42a925e851c115b0d8b950
DrDesten <>
2024-02-02 13:05:34 +0100

    updated build system to autogenerate world folders

--- build/parseProperties.js
+++ build/parseProperties.js
@@ -1,6 +1,6 @@
-const fs = require("fs")
+import fs from "fs"
 
-class PropertiesFile {
+export class PropertiesFile {
     constructor( path, source, statementPrefix ) {
         this.path = path
         this.source = source
@@ -27,24 +27,24 @@ class PropertiesFile {
         this.fileObject = targetData
 
         function parseLinebreaks( txt ) { // Replaces Line break escapes
-            return txt.replace(/\s*\\\s*\n/g, "")
+            return txt.replace( /\s*\\\s*\n/g, "" )
         }
         function parseComments( txt ) { // Removes Comments
-            return txt.replace(/#(?!if|ifdef|ifndef|elif|else|endif|define|undef).*$/gm, "")
+            return txt.replace( /#(?!if|ifdef|ifndef|elif|else|endif|define|undef).*$/gm, "" )
         }
         function trimSpaces( txt ) { // Removes excess spaces and newlines
-            return txt.replace(/[ \t]+/g, " ").replace(/\s*\n\s*/g, "\n").trim()
+            return txt.replace( /[ \t]+/g, " " ).replace( /\s*\n\s*/g, "\n" ).trim()
         }
-        
+
         function parseScope( txt ) { // Splits the code up into sections
 
             let sections = []
-            for (const line of txt.split("\n")) {
-                let lineType = getLineType(line)
-                if ( lineType == sections[ sections.length - 1 ]?.type ) {
-                    sections[ sections.length - 1 ].push( line )
+            for ( const line of txt.split( "\n" ) ) {
+                let lineType = getLineType( line )
+                if ( lineType == sections[sections.length - 1]?.type ) {
+                    sections[sections.length - 1].push( line )
                 } else {
-                    sections.push(Object.assign([ line ], { type: lineType }))
+                    sections.push( Object.assign( [line], { type: lineType } ) )
                 }
             }
 
@@ -71,33 +71,33 @@ class PropertiesFile {
                             emissive: /^\.?emissive\s*/,
                         }
                         const tagParser = {
-                            id: statement => (tags.id = tagRegex.id.exec(statement)[1], statement.replace(tagRegex.id, "")),
-                            data: statement => (tags.data = tagRegex.data.exec(statement)[1], statement.replace(tagRegex.data, "")),
-                            emissive: statement => (tags.emissive = 1, statement.replace(tagRegex.emissive, "")),
+                            id: statement => ( tags.id = tagRegex.id.exec( statement )[1], statement.replace( tagRegex.id, "" ) ),
+                            data: statement => ( tags.data = tagRegex.data.exec( statement )[1], statement.replace( tagRegex.data, "" ) ),
+                            emissive: statement => ( tags.emissive = 1, statement.replace( tagRegex.emissive, "" ) ),
                         }
 
                         let statement = section[i]
-                        let tag = tagRegex.tag.exec(statement)?.[1]
-                        if (!tag) throw new Error(`PropertiesFile > parseData() : No tag matched in '${statement}'`)
+                        let tag = tagRegex.tag.exec( statement )?.[1]
+                        if ( !tag ) throw new Error( `PropertiesFile > parseData() : No tag matched in '${statement}'` )
 
-                        while ( tagRegex.tag.test(statement) ) {
-                            let tag   = tagRegex.tag.exec(statement)[1]
-                            statement = tagParser[ tag ]( statement )
+                        while ( tagRegex.tag.test( statement ) ) {
+                            let tag = tagRegex.tag.exec( statement )[1]
+                            statement = tagParser[tag]( statement )
                         }
-                        if ( statement[0] != "=" ) throw new Error("Expected '=' at end of tag list")
+                        if ( statement[0] != "=" ) throw new Error( "Expected '=' at end of tag list" )
 
-                        statement = statement.replace(/^=\s*/, "")
-                        let targets = statement.split(/\s+/)
+                        statement = statement.replace( /^=\s*/, "" )
+                        let targets = statement.split( /\s+/ )
                         for ( const target of targets ) {
                             targetSection[target] ??= {}
-                            targetSection[target].id       ??= tags.id
-                            targetSection[target].data     ??= tags.data
+                            targetSection[target].id ??= tags.id
+                            targetSection[target].data ??= tags.data
                             targetSection[target].emissive ??= tags.emissive
                         }
                     }
-                    targetSections.push(targetSection)
+                    targetSections.push( targetSection )
                 } else if ( section.type == "preprocessor" ) {
-                    targetSections.push(section)
+                    targetSections.push( section )
                 }
             }
             return targetSections
@@ -113,41 +113,41 @@ class PropertiesFile {
             if ( section.type == "data" ) {
                 let compiledSection = {}
                 for ( const target in section ) {
-                    if ( target == "type" ) continue;
-                    let targetObj = section[ target ]
+                    if ( target == "type" ) continue
+                    let targetObj = section[target]
                     // Handle out-of-rangee values
-                    if ( targetObj.id > 255 )     console.warn(`WARNING: '${target}': tag 'id' out of range (${targetObj.id}, range: [0-255])`)
-                    if ( targetObj.emissive > 1 ) console.warn(`WARNING: '${target}': tag 'emissive' out of range (${targetObj.emissive}, range: [boolean])`)
-                    if ( targetObj.data > 63 )    console.warn(`WARNING: '${target}': tag 'data' out of range (${targetObj.data}, range: [0-63])`)
+                    if ( targetObj.id > 255 ) console.warn( `WARNING: '${target}': tag 'id' out of range (${targetObj.id}, range: [0-255])` )
+                    if ( targetObj.emissive > 1 ) console.warn( `WARNING: '${target}': tag 'emissive' out of range (${targetObj.emissive}, range: [boolean])` )
+                    if ( targetObj.data > 63 ) console.warn( `WARNING: '${target}': tag 'data' out of range (${targetObj.data}, range: [0-63])` )
 
                     // Pack values
-                    let packed    = packingFunction( targetObj.id, targetObj.emissive, targetObj.data )
-                    if ( compiledSection[ packed ] == undefined ) compiledSection[ packed ] = []
-                    compiledSection[ packed ].push( target )
+                    let packed = packingFunction( targetObj.id, targetObj.emissive, targetObj.data )
+                    if ( compiledSection[packed] == undefined ) compiledSection[packed] = []
+                    compiledSection[packed].push( target )
                 }
-                compiledSections.push(compiledSection)
+                compiledSections.push( compiledSection )
             }
         }
-        
+
         let compiledString = ""
         for ( const section of compiledSections ) {
             if ( section.type == "preprocessor" ) compiledString += section[0] + "\n"
             else compiledString += Object.keys( section ).map( key => {
-                return `${this.prefix}.${key}=${section[key].join(" ")}`
-            }).join("\n") + "\n"
+                return `${this.prefix}.${key}=${section[key].join( " " )}`
+            } ).join( "\n" ) + "\n"
         }
 
         return compiledString
     }
 
-    static pack( bits = [ 8, 1, 7 ] ) {
-        console.log(`pack(): Building Packing Function for ${bits.length} data points, to a ${bits.reduce((acc,curr)=>acc+curr,0)} bit integer`)
+    static pack( bits = [8, 1, 7] ) {
+        console.log( `pack(): Building Packing Function for ${bits.length} data points, to a ${bits.reduce( ( acc, curr ) => acc + curr, 0 )} bit integer` )
 
     }
 
 }
 
-class PropertiesParser {
+export class PropertiesParser {
     /** @param {string} text */
     constructor( text ) {
         /** @type {string} */
@@ -157,62 +157,62 @@ class PropertiesParser {
     /** @returns {{properties: string[], targets: string[]}[]} */
     parse() {
 
-        this.text = this.text.replace(/[\t ]*\\[\t ]*\n/g, "")
-        this.text = this.text.replace(/#.*/g, "")
-        this.text = this.text.replace(/\s*\n\s*/g, "\n").replace(/[\t ]+/, " ").trim()
+        this.text = this.text.replace( /[\t ]*\\[\t ]*\n/g, "" )
+        this.text = this.text.replace( /#.*/g, "" )
+        this.text = this.text.replace( /\s*\n\s*/g, "\n" ).replace( /[\t ]+/, " " ).trim()
 
-        const lines       = this.text.split("\n")
+        const lines = this.text.split( "\n" )
         const parsedLines = []
         for ( const line of lines ) {
-            let properties = /^([^=]*)=/.exec(line)[1]
-            let targets = /^[^=]*=(.*)/.exec(line)[1]
+            let properties = /^([^=]*)=/.exec( line )[1]
+            let targets = /^[^=]*=(.*)/.exec( line )[1]
 
-            properties = properties.split(".")
-            targets = targets.split(" ")
+            properties = properties.split( "." )
+            targets = targets.split( " " )
 
-            parsedLines.push({
+            parsedLines.push( {
                 properties: properties,
                 targets: targets
-            })
+            } )
         }
 
         return parsedLines
 
     }
 }
-class PropertiesCompiler {
+export class PropertiesCompiler {
     constructor( text ) {
-        this.lines = new PropertiesParser(text).parse()
+        this.lines = new PropertiesParser( text ).parse()
     }
 
     parseProperties() {
-        for (let i = 0; i < this.lines.length; i++) {
+        for ( let i = 0; i < this.lines.length; i++ ) {
 
             const line = this.lines[i]
             const properties = line.properties
 
             const parsed = []
             let token
-            while( properties.length > 0 ) {
-                const nextToken = properties.splice(0,1)
-                if (nextToken == "emissive") {
-                    parsed.push({ property: "emissive", value: 1 })
+            while ( properties.length > 0 ) {
+                const nextToken = properties.splice( 0, 1 )
+                if ( nextToken == "emissive" ) {
+                    parsed.push( { property: "emissive", value: 1 } )
                     token = undefined
                     continue
                 }
-                if (nextToken == "id") {
+                if ( nextToken == "id" ) {
                     token = { property: "id", value: null }
                     continue
                 }
-                if (nextToken == "data") {
+                if ( nextToken == "data" ) {
                     token = { property: "data", value: null }
                     continue
                 }
 
-                if (isFinite(+nextToken)) {
-                    if (!token) throw new Error(`Cannot assign value to undefined property`)
+                if ( isFinite( +nextToken ) ) {
+                    if ( !token ) throw new Error( `Cannot assign value to undefined property` )
                     token.value = +nextToken
-                    parsed.push(token)
+                    parsed.push( token )
                     token = undefined
                 }
             }
@@ -224,25 +224,25 @@ class PropertiesCompiler {
     }
 
     orientTarget() {
-        
+
         const targets = []
-        for (let i = 0; i < this.lines.length; i++) {
+        for ( let i = 0; i < this.lines.length; i++ ) {
             const line = this.lines[i]
-            for (const target of line.targets) {
+            for ( const target of line.targets ) {
                 const index = targets.findIndex( ele => ele.target == target )
 
-                if (index == -1) {
-                    targets.push({
+                if ( index == -1 ) {
+                    targets.push( {
                         target: target,
                         properties: line.properties
-                    })
+                    } )
                     continue
                 }
 
-                for (const property of line.properties) {
+                for ( const property of line.properties ) {
                     const propertyIndex = targets[index].properties.findIndex( prop => prop.property == property.property )
 
-                    if (propertyIndex == -1) {
+                    if ( propertyIndex == -1 ) {
                         targets[index].properties.push( property )
                         continue
                     }
@@ -252,7 +252,7 @@ class PropertiesCompiler {
 
             }
         }
-        
+
         this.targets = targets
         return this
     }
@@ -260,34 +260,25 @@ class PropertiesCompiler {
 }
 
 function packData( id, emissive, data ) {
-    let out = 0;
-    out  = ~~data << 9
+    let out = 0
+    out = ~~data << 9
     out |= ~~emissive << 8
     out |= ~~id
     return out
 }
 
 
-const fext  = path => path.match(/.*\.(\w+)$/)?.[1]
-const fname = path => path.match(/.*\/([\w\.]*)\.\w+$/)?.[1]
-const ffull = path => `${fname(path)}.${fext(path)}`
+const fext = path => path.match( /.*\.(\w+)$/ )?.[1]
+const fname = path => path.match( /.*\/([\w\.]*)\.\w+$/ )?.[1]
+const ffull = path => `${fname( path )}.${fext( path )}`
 
 /** @param {string} path */
-function loadProperties( path ) {
-    let content = fs.readFileSync(path, {encoding: "utf8"})
-    return new PropertiesFile( path, content, fname(path) )
+export function loadProperties( path ) {
+    let content = fs.readFileSync( path, { encoding: "utf8" } )
+    return new PropertiesFile( path, content, fname( path ) )
 }
 /** @param {PropertiesFile} propertiesFile */
-function compileProperties( propertiesFile ) {
-    let compiled = propertiesFile.compileFromTargets(packData)
-    fs.writeFileSync(`${propertiesFile.path}`, compiled )
-}
-
-
-module.exports = {
-    PropertiesFile,
-    PropertiesParser,
-    PropertiesCompiler,
-    loadProperties,
-    compileProperties,
+export function compileProperties( propertiesFile ) {
+    let compiled = propertiesFile.compileFromTargets( packData )
+    fs.writeFileSync( `${propertiesFile.path}`, compiled )
 }
