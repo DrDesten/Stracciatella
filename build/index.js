9022add8d375899119246c094be25f3f4a365ffd
DrDesten <>
2024-08-22 17:57:09 +0200

    fully refactored build script

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 import path from "path"
 import url from "url"
-import { guardFiles } from "./generateIncludeGuards.js"
+import { guardFiles } from "./guards.js"
 import { guardUniforms } from "./parseUniforms.js"
 import { PropertiesFile, PropertiesParser, PropertiesCompiler, loadProperties, compileProperties } from "./parseProperties.js"
 import { FileMapping } from "./filemap.js"
@@ -16,6 +16,12 @@ const __dirname = path.dirname( url.fileURLToPath( import.meta.url ) )
 const src = path.join( __dirname, "../src" )
 const shaders = path.join( __dirname, "../shaders" )
 const changes = new Changes( src )
+
+// Force Rebuild
+if ( process.argv[2] === "-f" || process.argv[2] === "--force" ) {
+    fs.rmSync( shaders, { recursive: true, force: true } )
+    changes.clearCache()
+}
 
 // copy all files
 changes.addChangeListener( "*", filepath => {
@@ -40,54 +46,21 @@ changes.addChangeListener( ["/*.fsh", "/*.vsh", "/*.gsh"], filepath => {
     }
 } )
 
-await changes.apply()
-
-/** @param {string} dir @returns {string[]} */
-function gatherFiles( dir, fileList = [] ) {
-    // Get all Files and Directories in the current folder
-    let files = fs.readdirSync( dir )
-        .map( path => `${dir}/${path}` )
-        .filter( path => fs.lstatSync( path ).isDirectory() || fs.lstatSync( path ).isFile() )
-    // Recursively add files in subdirectories ( && is used for short-circuiting )
-    files.forEach( path => fs.lstatSync( path ).isDirectory() && gatherFiles( path, fileList ) )
-    // Filter out Directories, as everything should have resolved now
-    files = files.filter( path => fs.lstatSync( path ).isFile() )
-    // Add all found files to the referenced array
-    fileList.push( ...files )
-    return fileList
-}
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Compile Files
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const files = gatherFiles( shaders )
+changes.addChangeListener( ["*.fsh", "*.vsh", "*.gsh", "*.glsl"], filepath => {
+    const dstpath = path.join( shaders, filepath )
+    guardFiles( dstpath )
+    guardUniforms( dstpath )
+    console.log( `Compiled ${filepath}` )
+} )
+changes.addChangeListener( ["block.properties", "item.properties", "entity.properties"], filepath => {
+    const dstpath = path.join( shaders, filepath )
+    const propertiesFile = loadProperties( dstpath )
+    compileProperties( propertiesFile )
+    console.log( `Compiled ${filepath}` )
+} )
 
-for ( const filepath of files ) {
-
-    const fext = path => path.match( /.*\.(\w+)$/ )?.[1]
-    const fname = path => path.match( /.*\/([\w\.]*)\.\w+$/ )?.[1]
-    const ffull = path => `${fname( path )}.${fext( path )}`
-
-    const indir = ( path, dir ) => typeof dir == "string" ? path.includes( dir ) : dir.test( path )
-
-    if ( indir( filepath, "shaders/core" ) )
-        continue
-
-    switch ( path.extname( filepath ) ) {
-        case ".properties":
-            if ( ["block", "item", "entity"].includes( path.basename( filepath, ".properties" ) ) ) {
-                //new PropertiesCompiler(fs.readFileSync(path, {encoding: "utf8"})).parseProperties().orientTarget()
-                const propertiesFile = loadProperties( filepath )
-                //console.log(propertiesFile.fileObject)
-                compileProperties( propertiesFile )
-            }
-            break
-        case ".fsh":
-        case ".vsh":
-        case ".glsl":
-            if ( !indir( filepath, /world-?\d/ ) ) guardFiles( filepath )
-            guardUniforms( filepath )
-            break
-    }
-}
+await changes.apply()
