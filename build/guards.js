1beab9b41ba519d108485437fe913c421465379c
DrDesten <>
2024-08-27 01:35:40 +0200

    build stuff 2

--- build/guards.js
+++ build/guards.js
@@ -3,7 +3,7 @@ import path from "path"
 import crypto from "crypto"
 
 export function guardFiles( filepath ) {
-    let content = fs.readFileSync( filepath, { encoding: "utf-8" } )
+    let content = fs.readFileSync( filepath )
     const hash = crypto.createHash( "sha1" ).update( filepath ).digest( "hex" ).slice( 0, 4 )
     const define = `INCLUDE_${path.basename( filepath ).replace( /\W+/g, "_" )}_${hash}`.toUpperCase()
     content = `#if ! defined ${define}\n#define ${define}\n\n${content}\n\n#endif`
