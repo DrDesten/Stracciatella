b7ed6b327fcb71ad54ab7a248a64778936dc0802
DrDesten <70536771+DrDesten@users.noreply.github.com>
2024-09-01 11:01:11 +0000

    updated core, fixed argv.js

--- build/argv.js
+++ build/argv.js
@@ -7,38 +7,55 @@
 export function parseArgv( schema, argv, slice = 2 ) {
     argv = argv.slice( slice )
 
+    // Resolve subcommand
     let options = schema
     let command = []
     while ( argv.length && typeof options[argv[0]] === "object" )
         options = options[argv[0]], command.push( argv.shift() )
+    schema.command = command.join( " " )
 
+    if (argv.length && !argv[0].startsWith("-")) {
+        console.warn(`Unknown command "${[...command, argv[0]].join(" ")}"`)
+        return schema
+    }
+
+    // Filter out subcommands from options
+    let keys = Object.keys(options).filter(key => typeof options[key] !== "object")
+    let fullkeys = Object.fromEntries(keys.map(key => [key, key]))
+    let shortkeys = Object.fromEntries(keys.map(key => [key[0], key]).reverse())
     while ( argv.length ) {
         const input = argv.shift()
         if ( input.startsWith( "--" ) ) { // --option / --no-option
             const value = !input.startsWith( "--no-" )
             const arg = input.slice( value ? 2 : 5 )
-            if ( arg in options ) options[arg] = value
+
+            if ( !(arg in fullkeys) ) {
+                console.warn(`Unknown option "${arg}"`)
+                continue
+            }
+            
+            options[arg] = value
             continue
         }
         if ( input.startsWith( "-" ) ) { // -o / -no-o
-            const candidates = Object.fromEntries(
-                Object.keys( options ).map( opt => [opt[0], opt] )
-                    .filter( ( [opt], i, arr ) => arr.findIndex( ( [x] ) => x === opt ) === i )
-            )
-
             const value = !input.startsWith( "-no-" )
             const args = input.slice( value ? 1 : 4 ).split( "" )
 
-            if ( new Set( args ).size !== args.length || !args.every( o => o in candidates ) ) {
-                console.error( "Unknown option:", input, candidates, args )
+            if ( !args.every( o => o in shortkeys ) ) {
+                const unknowns = [...new Set(args)].filter(o => !(o in shortkeys))
+                console.warn( `Unknown options: ${unknowns.map(o => `"${o}"`)}` )
+                continue
+            }
+            if ( new Set(args).size !== args.length ) {
+                const duplicates = args.filter((o,i,a) => a.indexOf(o) !== i)
+                console.warn(`Duplicate options: ${duplicates.map(o => `"${o}"`)}`)
                 continue
             }
 
-            for ( const arg of args ) options[candidates[arg]] = value
+            for ( const arg of args ) options[shortkeys[arg]] = value
             continue
         }
     }
 
-    schema.command = command.join( " " )
     return schema
 }
