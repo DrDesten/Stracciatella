a1eaa0dcefcb8574d38f1dd013a1c080c72a9dd5
DrDesten <70536771+DrDesten@users.noreply.github.com>
2024-09-09 18:36:50 +0200

    agressive optimisation is now default OFF

--- src/lib/vertex_transform_simple.glsl
+++ src/lib/vertex_transform_simple.glsl
@@ -6,13 +6,25 @@ vec3 getNormal() {
     return normalize(gl_NormalMatrix * gl_Normal);
 }
 
+#ifdef OPT_UNSAFE
 vec2 getCoord() {
     return mat2(gl_TextureMatrix[0]) * gl_MultiTexCoord0.xy + gl_TextureMatrix[0][3].xy;
 }
+#else
+vec2 getCoord() {
+    return (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
+}
+#endif
 
+#ifdef OPT_SAFE
 vec2 getLmCoord() {
     return gl_MultiTexCoord1.xy * (1./240);
 }
+#else 
+vec2 getLmCoord() {
+    return (gl_TextureMatrix[1] * gl_MultiTexCoord1).xy;
+}
+#endif
 
 mat3 getTBN(vec4 tangentAttribute) {
 	vec3 normal  = normalize(gl_NormalMatrix * gl_Normal);
