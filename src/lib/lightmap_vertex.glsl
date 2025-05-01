e26a0b643c57a51343b263dda7a36a818379aaec
DrDesten <>
2025-05-01 23:23:32 +0200

    fix implicit int->float conversion

--- src/lib/lightmap_vertex.glsl
+++ src/lib/lightmap_vertex.glsl
@@ -2,7 +2,7 @@ uniform vec3 up;
 
 float getTerrainShading(vec3 glNormal, int blockId) {
     bool isExempt = blockId == 10 || blockId == 11 || blockId == 17 || blockId == 12;
-    if (isExempt) return 1;
+    if (isExempt) return 1.;
     return dot(normalize(vec3(.25, 1, .5)), glNormal) * 0.15 + 0.85;
 }
 
